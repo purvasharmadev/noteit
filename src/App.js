a71cd7869f26b75440610df5648a446093082a6b
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-19 19:24:05 +0530

    docs:added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
-import URLRoutes from "./routes"
+import URLRoutes from "./routes";
 
 function App() {
-  const apiKey = process.env.REACT_APP_JWT_SECRET
-  console.log(apiKey)
+  const apiKey = process.env.REACT_APP_JWT_SECRET;
+  console.log(apiKey);
   return (
-    <div className="App">
-      <h1>NoteIt</h1>
-      <URLRoutes/>
-    </div>
+    <>
+      <URLRoutes />
+    </>
   );
 }
 
