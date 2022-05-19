e322c45422b365dc055f13a684454bdc83a5ebc4
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-19 19:24:33 +0530

    feat: added hooks

---
+++ src/Hooks/useAxios.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
+function useAxios() {
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  async function fetchData(params) {
+    try {
+      let result = await axios.request(params);
+      setResponse(result.data);
+    } catch (error) {
+      console.error(error.response.data.errors[0]);
+      setError(error.response.data.errors[0]);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { response, loading, error, fetchData };
+}
+
+export { useAxios };
